4c96770433adbcbe777297e73583fd24623b28b0
fehbarea <fehbarea@icloud.com>
2025-01-30 23:22:05 -0300

    criacao login

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,11 +1,45 @@
-const userModel = require('../models/UserModel.js');
+import pool from '../config/db.js';
+import jwt from 'jsonwebtoken';
 
-login = async (req, res, next) => {
-    const { usuario, senha } = req.body;
+function authentication(username, password) {
 
-    const token = await userModel.login(usuario, senha);
-    
-    if(token == null){
+    const query = {
+        text: 'SELECT * FROM users WHERE username = $1 AND password = $2',
+        values: [username, password]
+    };
+    const rs = pool.query(query);
+
+    const queryPermissions = {
+        text: 'SELECT * FROM cargos_permissoes WHERE cargo_id = $1',
+        values: [rs[0].cargo_id]
+    };
+
+    if (rs.length > 0) {
+        const permissions = pool.query(queryPermissions);
+        return tokenGeneration(rs[0].id, rs[0].username, permissions);
+    }
+    return null;
+
+}
+
+function tokenGeneration(id, username, permissions) {
+
+    const payload = {
+        id: id,
+        username: username,
+        permissions: permissions
+    };
+
+    let secret = process.env.JWT_SECRET || 'default';
+    let expiresIn = process.env.JWT_EXPIRES_IN || '1800s';
+    return jwt.sign(payload, secret, { expiresIn: expiresIn });
+}
+
+const login = async (req, res, next) => {
+    const { username, password } = req.body;
+
+    const token = authentication(username, password);
+    if (token == null) {
         next(new CustomError(401, 'Credenciais inválidas.'));
     }
     res.status(200).json({ token });
--- src/routes/AuthRoutes.js
+++ src/routes/AuthRoutes.js
@@ -5,4 +5,4 @@ const router = express.Router();
 
 router.post('/', authController);
 
-module.exports = router;
+export default router;
