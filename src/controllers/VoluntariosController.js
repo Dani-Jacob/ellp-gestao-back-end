098bfe01da7cc705edb232183787ac889b305cfa
Daniel Jacob <danieljaroszczuk28@gmail.com>
2025-02-11 16:37:00 -0300

    Criação do models de voluntarios

--- src/controllers/VoluntariosController.js
+++ src/controllers/VoluntariosController.js
@@ -1,17 +1,26 @@
 import pool from '../config/db.js';
 import argon2 from 'argon2';
 
+import {
+    getVoluntarioByIdModel,
+    getVoluntarioByCpfModel,
+    getVoluntarioByEmailModel,
+    createVoluntarioModel,
+    getAllVoluntariosModel,
+    updateVoluntarioModel,
+    deleteVoluntarioModel,
+    addFrequenciaVoluntarioAtividadeModel,
+    addFrequenciaVoluntarioAulaModel
+} from '../models/VoluntariosModels.js';
 
 async function createVoluntario(req, res) {
     const { nome, ra, telefone, cpf, email, curso, endereco, bairro, cep, senha, cargo_id, id_departamento } = req.body;
 
-    const existingVoluntario = await pool.query('SELECT * FROM voluntarios WHERE cpf = $1', [cpf]);
-    if (existingVoluntario.rows.length > 0) {
+    if ((await getVoluntarioByCpfModel(cpf)).rows.length > 0) {
         return res.status(400).json({ message: "Já existe um voluntário com esse CPF." });
     }
 
-    const existingEmail = await pool.query('SELECT * FROM voluntarios WHERE email = $1', [email]);
-    if (existingEmail.rows.length > 0) {
+    if ((await getVoluntarioByEmailModel(email)).rows.length > 0) {
         return res.status(400).json({ message: "Já existe um voluntário com esse e-mail." });
     }
 
@@ -27,40 +36,19 @@ async function createVoluntario(req, res) {
 
     const hashedPassword = await argon2.hash(senha, { type: argon2.argon2id });
 
-    const result = await pool.query(`
-        INSERT INTO voluntarios (
-            nome, ra, telefone, cpf, email, curso, endereco, bairro, cep, senha, cargo_id, id_departamento
-        )
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
-        RETURNING *
-    `, [nome, ra, telefone, cpf, email, curso, endereco, bairro, cep, hashedPassword, cargo_id, id_departamento]);
+    const result = await createVoluntarioModel(nome, ra, telefone, cpf, email, curso, endereco, bairro, cep, hashedPassword, cargo_id, id_departamento);
 
     res.status(201).json(result.rows[0]);
 }
 
 async function getAllVoluntarios(req, res) {
-    const result = await pool.query(`SELECT v.id AS id_voluntario, v.nome AS nome_voluntario, v.ra, v.telefone, v.cpf, v.email, v.curso, v.ativo, 
-       v.endereco, v.bairro, v.cep, v.senha, v.data_cadastro, 
-       c.id AS id_cargo, c.nome AS nome_cargo, 
-       d.id AS id_departamento, d.nome AS nome_departamento
-        FROM voluntarios v
-        LEFT JOIN cargos c ON v.cargo_id = c.id
-        LEFT JOIN departamentos d ON v.id_departamento = d.id;`);
+    const result = await getAllVoluntariosModel();
     res.status(200).json(result.rows);
 }
 
 async function getVoluntarioById(req, res) {
     const { id } = req.params;
-    const result = await pool.query(`
-        SELECT v.id AS id_voluntario, v.nome AS nome_voluntario, v.ra, v.telefone, v.cpf, v.email, v.curso, v.ativo, 
-       v.endereco, v.bairro, v.cep, v.senha, v.data_cadastro, 
-       c.id AS id_cargo, c.nome AS nome_cargo, 
-       d.id AS id_departamento, d.nome AS nome_departamento
-        FROM voluntarios v
-        LEFT JOIN cargos c ON v.cargo_id = c.id
-        LEFT JOIN departamentos d ON v.id_departamento = d.id
-        WHERE v.id = $1;`, [id]);
-
+    const result = await getVoluntarioByIdModel(id);
     if (result.rows.length === 0) {
         return res.status(404).json({ message: "Voluntário não encontrado." });
     }
@@ -72,43 +60,23 @@ async function updateVoluntario(req, res) {
     const { id } = req.params;
     const { nome, ra, telefone, cpf, email, curso, endereco, bairro, cep, senha, cargo_id, id_departamento } = req.body;
 
-    const voluntario = await pool.query('SELECT * FROM voluntarios WHERE id = $1', [id]);
-    if (voluntario.rows.length === 0) {
+    if ((await getVoluntarioById(id)).rows.length === 0) {
         return res.status(404).json({ message: "Voluntário não encontrado." });
     }
 
-    const result = await pool.query(`
-        UPDATE voluntarios
-        SET 
-            nome = $1,
-            ra = $2,
-            telefone = $3,
-            cpf = $4,
-            email = $5,
-            curso = $6,
-            endereco = $7,
-            bairro = $8,
-            cep = $9,
-            senha = $10,
-            cargo_id = $11,
-            id_departamento = $12
-        WHERE id = $13
-        RETURNING *
-    `, [nome, ra, telefone, cpf, email, curso, endereco, bairro, cep, senha, cargo_id, id_departamento, id]);
+    const result = await updateVoluntarioModel(id, nome, ra, telefone, cpf, email, curso, endereco, bairro, cep, senha, cargo_id, id_departamento, id);
 
     res.status(200).json(result.rows[0]);
 }
 
-
 async function deleteVoluntario(req, res) {
     const { id } = req.params;
 
-    const voluntario = await pool.query('SELECT * FROM voluntarios WHERE id = $1', [id]);
-    if (voluntario.rows.length === 0) {
+    if ((await getVoluntarioByIdModel(id)).rows.length === 0) {
         return res.status(404).json({ message: "Voluntário não encontrado." });
     }
 
-    await pool.query('DELETE FROM voluntarios WHERE id = $1', [id]);
+    await deleteVoluntarioModel(id);
 
     res.status(200).json({ message: "Voluntário deletado com sucesso." });
 }
@@ -116,8 +84,7 @@ async function deleteVoluntario(req, res) {
 async function addFrequenciaVoluntarioAtividade(req, res) {
     const { id, id_atividade } = req.params;
 
-    const voluntario = await pool.query('SELECT * FROM voluntarios WHERE id = $1', [id]);
-    if (voluntario.rows.length === 0) {
+    if ((await getVoluntarioById(id)).rows.length == 0) {
         return res.status(404).json({ message: "Voluntário não encontrado." });
     }
 
@@ -126,21 +93,15 @@ async function addFrequenciaVoluntarioAtividade(req, res) {
         return res.status(404).json({ message: "Atividade não encontrada." });
     }
 
-    const rs = await pool.query(`
-        INSERT INTO frequencia_voluntarios_atividades (voluntario_id, atividade_id)
-        VALUES ($1,$2)
-        `,[id,id_atividade]);
+    const rs = await addFrequenciaVoluntarioAtividadeModel(id, id_atividade);
         	
     res.status(201).json({ message: "Frequência registrada com sucesso!"});
 }
 
-
-
 async function addFrequenciaVoluntarioAula(req, res) {
     const { id, id_aula } = req.params;
 
-    const voluntario = await pool.query('SELECT * FROM voluntarios WHERE id = $1', [id]);
-    if (voluntario.rows.length === 0) {
+    if ((await getVoluntarioById(id)).rows.length == 0) {
         return res.status(404).json({ message: "Voluntário não encontrado." });
     }
 
@@ -149,10 +110,7 @@ async function addFrequenciaVoluntarioAula(req, res) {
         return res.status(404).json({ message: "Aula não encontrada." });
     }
 
-    const rs = await pool.query(`
-        INSERT INTO frequencia_voluntarios_aulas (voluntario_id, aula_id)
-        VALUES ($1,$2)
-        `,[id,id_aula]);
+    const rs = await addFrequenciaVoluntarioAulaModel(id, id_aula);
         	
     res.status(201).json({ message: "Frequência registrada com sucesso!"});
 }
---
+++ src/models/VoluntariosModels.js
@@ -0,0 +1,124 @@
+import pool from '../config/db.js';
+import argon2 from 'argon2';
+
+async function getVoluntarioByIdModel(id) {
+    const result = await pool.query(`
+        SELECT v.id AS id_voluntario, v.nome AS nome_voluntario, v.ra, v.telefone, v.cpf, v.email, v.curso, v.ativo, 
+       v.endereco, v.bairro, v.cep, v.senha, v.data_cadastro, 
+       c.id AS id_cargo, c.nome AS nome_cargo, 
+       d.id AS id_departamento, d.nome AS nome_departamento
+        FROM voluntarios v
+        LEFT JOIN cargos c ON v.cargo_id = c.id
+        LEFT JOIN departamentos d ON v.id_departamento = d.id
+        WHERE v.id = $1;`, [id]);
+
+    return result;
+}
+
+async function getVoluntarioByCpfModel(cpf) {
+    const result = await pool.query('SELECT * FROM voluntarios WHERE cpf = $1', [cpf]);
+    return result;
+}
+
+async function getVoluntarioByEmailModel(email) {
+    const result = await pool.query('SELECT * FROM voluntarios WHERE email = $1', [email]);
+    return result;
+}
+
+async function createVoluntarioModel(nome, ra, telefone, cpf, email, curso, endereco, bairro, cep, senha, cargo_id, id_departamento) {
+    const result = await pool.query(`
+        INSERT INTO voluntarios (
+            nome, ra, telefone, cpf, email, curso, endereco, bairro, cep, senha, cargo_id, id_departamento
+        )
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
+        RETURNING *
+    `, [nome, ra, telefone, cpf, email, curso, endereco, bairro, cep, hashedPassword, cargo_id, id_departamento]);
+
+    return result;
+}
+
+async function getAllVoluntariosModel() {
+    const result = await pool.query(`SELECT v.id AS id_voluntario, v.nome AS nome_voluntario, v.ra, v.telefone, v.cpf, v.email, v.curso, v.ativo, 
+       v.endereco, v.bairro, v.cep, v.senha, v.data_cadastro, 
+       c.id AS id_cargo, c.nome AS nome_cargo, 
+       d.id AS id_departamento, d.nome AS nome_departamento
+        FROM voluntarios v
+        LEFT JOIN cargos c ON v.cargo_id = c.id
+        LEFT JOIN departamentos d ON v.id_departamento = d.id;`);
+    return result;
+}
+
+
+
+async function updateVoluntarioModel(id,nome, ra, telefone, cpf, email, curso, endereco, bairro, cep, senha, cargo_id, id_departamento) {
+    const result = await pool.query(`
+        UPDATE voluntarios
+        SET 
+            nome = $1,
+            ra = $2,
+            telefone = $3,
+            cpf = $4,
+            email = $5,
+            curso = $6,
+            endereco = $7,
+            bairro = $8,
+            cep = $9,
+            senha = $10,
+            cargo_id = $11,
+            id_departamento = $12
+        WHERE id = $13
+        RETURNING *
+    `, [nome, ra, telefone, cpf, email, curso, endereco, bairro, cep, senha, cargo_id, id_departamento, id]);
+
+    return result;
+}
+
+
+async function deleteVoluntarioModel(id) {
+    const result = await pool.query('DELETE FROM voluntarios WHERE id = $1', [id]);
+
+    return result;
+}
+
+async function addFrequenciaVoluntarioAtividadeModel(id, id_atividade) {
+    const atividade = await pool.query('SELECT * FROM atividade WHERE id = $1', [id_atividade]);
+    if (atividade.rows.length === 0) {
+        return res.status(404).json({ message: "Atividade não encontrada." });
+    }
+
+    const rs = await pool.query(`
+        INSERT INTO frequencia_voluntarios_atividades (voluntario_id, atividade_id)
+        VALUES ($1,$2)
+        `,[id,id_atividade]);
+        	
+    return rs;
+}
+
+
+
+async function addFrequenciaVoluntarioAulaModel(id, id_aula) {
+
+    const atividade = await pool.query('SELECT * FROM aula WHERE id = $1', [id_aula]);
+    if (atividade.rows.length === 0) {
+        return res.status(404).json({ message: "Aula não encontrada." });
+    }
+
+    const rs = await pool.query(`
+        INSERT INTO frequencia_voluntarios_aulas (voluntario_id, aula_id)
+        VALUES ($1,$2)
+        `,[id,id_aula]);
+        	
+    return rs;
+}
+
+export {
+    getVoluntarioByIdModel,
+    getVoluntarioByCpfModel,
+    getVoluntarioByEmailModel,
+    createVoluntarioModel,
+    getAllVoluntariosModel,
+    updateVoluntarioModel,
+    deleteVoluntarioModel,
+    addFrequenciaVoluntarioAtividadeModel,
+    addFrequenciaVoluntarioAulaModel
+};
